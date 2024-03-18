c95d45721d284bb0340a423e724627d26c2d2a8b
Favour Emmanuel <iyenefavour@gmail.com>
2024-03-18 16:37:42 +0100

    created the UI for the table and their rows and colums

--- src/assets/ContactTable.tsx/Table.tsx
+++ src/assets/ContactTable.tsx/Table.tsx
@@ -1,7 +1,70 @@
 import React from "react";
 
 const Table = () => {
-  return <div>Table</div>;
+  return (
+    <div className="pt-2 mx-auto max-w-5xl">
+      <div className="flex justify-center my-4">
+        <h1 className="text-3xl font-extrabold">Contact Keeper</h1>
+      </div>
+      <div className="max-w-[30rem] bg-[#262626] rounded-lg mx-auto px-3 py-2 border border-[#AFAFAF]">
+        <input
+          type="search"
+          placeholder="Search contacts"
+          className="w-full bg-transparent outline-none text-sm font-normal"
+        />
+      </div>
+      {/* table */}
+
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg mx-auto mt-4 max-w-5xl">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                First name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Last name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Email
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Phone
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Luke
+              </th>
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Skywalker
+              </th>
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                email@email.com
+              </th>
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                234-567-8943
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Table;
